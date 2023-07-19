dbb87457900d1b651aef30fab54e2d53fae8f2f7
“Abhishek <kabhi12132002@gmail.com>
2023-07-19 20:45:03 +0530

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* header toggle */
+let MenuBtn =document.getElementById('MenuBtn')
+
+MenuBtn .addEventListener('click',function(e){
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
